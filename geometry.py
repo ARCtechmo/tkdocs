2e3c201dd6a005ca2bdd60d297f064ab70f4c1a1
ARCtechmo <arctitan78@gmail.com>
2023-04-16 10:18:08 -0400

    just some notes on Tkinter geometry

---
+++ geometry.py
@@ -0,0 +1,7 @@
+# http://tkdocs.com/tutorial/concepts.html#geometry
+# the geometry manager determines where widgets are placed. 
+
+# *** pack() method is out of date ***
+# *** use grid() method ***
+
+# use the grid() method to tell the geometry manager what child to manage within the parent
